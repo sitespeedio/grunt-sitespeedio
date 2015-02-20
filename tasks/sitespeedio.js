bad5897f06cd41e1b015dade38185933ffa425f0
soulgalore <peter@soulgalore.com>
2015-02-20 12:58:11 +0100

    if we use the --file flag, read the file from disk #2

--- tasks/sitespeedio.js
+++ tasks/sitespeedio.js
@@ -7,73 +7,93 @@
 
 'use strict';
 
+var fs = require('fs'),
+  path = require('path'),
+  EOL = require('os').EOL;
+
 module.exports = function(grunt) {
 
-	grunt.registerMultiTask('sitespeedio', 'Analyze your sites web performance', function() {
+  grunt.registerMultiTask('sitespeedio', 'Analyze your sites web performance', function() {
 
-		var tmp = require('temporary');
-		var dir = new tmp.Dir();
+    var tmp = require('temporary');
+    var dir = new tmp.Dir();
 
-		var options = this.options({
-			resultBaseDir: dir.path,
-			html: true
-		});
+    var options = this.options({
+      resultBaseDir: dir.path,
+      html: true
+    });
 
-		var done = this.async();
+    // special handling for reading file with urls
+    if (options.file) {
+      var fullPathToFile = (options.file.charAt(0) === path.sep) ? options.file : path.join(process.cwd(),
+        path.sep, options.file);
 
-		var Sitespeed = require('sitespeed.io/lib/sitespeed');
-		var sitespeed = new Sitespeed();
+      var data = fs.readFileSync(fullPathToFile);
+      var urls = data.toString().split(EOL);
+      urls = urls.filter(function(l) {
+        return l.length > 0;
+      });
+      options.urls = urls;
+      options.file = undefined;
+    }
 
-		sitespeed.run(options, function(err, data) {
+    var done = this.async();
 
-			// lets get the budget!
-			if (data && data.budget) {
+    var Sitespeed = require('sitespeed.io/lib/sitespeed');
+    var sitespeed = new Sitespeed();
 
-				grunt.log.ok('------------------------------------------------- Check budget');
-				var failing = false;
-				var includePassed = grunt.config.get('includePassed');
+    sitespeed.run(options, function(err, data) {
 
-				var noPassedTests = 0;
-				var noFailingTests = 0;
-				var noSkippedTests = 0;
+      // lets get the budget!
+      if (data && data.budget) {
 
-				grunt.log.ok((includePassed ? 'Will include passing tests.' : 'Will not include passing tests.') +
-					' Change this by set grunt config to includePassed to true/false');
+        grunt.log.ok('------------------------------------------------- Check budget');
+        var failing = false;
+        var includePassed = grunt.config.get('includePassed');
 
-				data.budget.forEach(function(result) {
+        var noPassedTests = 0;
+        var noFailingTests = 0;
+        var noSkippedTests = 0;
 
-					if (result.skipped) {
-						noSkippedTests++;
-						grunt.log.ok('Skipping ' + result.title + ' ' + result.url + ' ' + ' value [' + result.value + ']');
-					} else if (result.isOk) {
-						noPassedTests++;
-						if (includePassed) {
-							grunt.log.ok('The budget for ' + result.title + ' ' + result.url + ' passed [' + result.value + ']');
-						}
-					} else {
-						noFailingTests++;
-						failing = true;
-						grunt.log.error('The budget for ' + result.title + ' ' + result.url + ' failed. ' + result.description);
-					}
-				});
+        grunt.log.ok((includePassed ? 'Will include passing tests.' : 'Will not include passing tests.') +
+          ' Change this by set grunt config to includePassed to true/false');
 
-				grunt.log.ok('We got ' + noPassedTests + ' passing tests, ' + noFailingTests + ' failing' + ((noSkippedTests>0) ? ' ' + noSkippedTests + ' skipped tests': '.'));
+        data.budget.forEach(function(result) {
 
-				grunt.log.ok('------------------------------------------------- Finished checking budget');
+          if (result.skipped) {
+            noSkippedTests++;
+            grunt.log.ok('Skipping ' + result.title + ' ' + result.url + ' ' + ' value [' + result.value +
+              ']');
+          } else if (result.isOk) {
+            noPassedTests++;
+            if (includePassed) {
+              grunt.log.ok('The budget for ' + result.title + ' ' + result.url + ' passed [' + result.value +
+                ']');
+            }
+          } else {
+            noFailingTests++;
+            failing = true;
+            grunt.log.error('The budget for ' + result.title + ' ' + result.url + ' failed. ' + result.description);
+          }
+        });
 
-				if (failing) {
-					done(false);
-				} else {
-					done();
-				}
-			}
+        grunt.log.ok('We got ' + noPassedTests + ' passing tests, ' + noFailingTests + ' failing' + ((
+          noSkippedTests > 0) ? ' ' + noSkippedTests + ' skipped tests' : '.'));
 
-			if (err) {
-				done(false);
-			}
-			else {
-				done();
-			}
-		});
-	});
+        grunt.log.ok('------------------------------------------------- Finished checking budget');
+
+        if (failing) {
+          done(false);
+        } else {
+          done();
+        }
+      }
+
+      if (err) {
+        done(false);
+      } else {
+        done();
+      }
+    });
+  });
 };
