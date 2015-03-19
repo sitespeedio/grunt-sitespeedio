cc65f45abee9e9b52e2afc26b59939c34ba08f8d
zenderol <oleg.zender@asideas.de>
2015-03-19 14:38:24 +0100

    cleanup code formatting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,42 +1,43 @@
- /**
-  * grunt-sitespeed.io (http://www.sitespeed.io)
-  * Copyright (c) 2015, Peter Hedenskog, Tobias Lidskog
-  * and other contributors
-  * Released under the Apache 2.0 License
-  */
- 'use strict';
+/**
+ * grunt-sitespeed.io (http://www.sitespeed.io)
+ * Copyright (c) 2015, Peter Hedenskog, Tobias Lidskog
+ * and other contributors
+ * Released under the Apache 2.0 License
+ */
+'use strict';
 
- module.exports = function(grunt) {
+module.exports = function(grunt) {
 
-     // Project configuration.
-     grunt.initConfig({
-         // Before generating any new files, remove any previously-created files.
-         clean: {
-             tests: ['tmp']
-         },
-         nodeunit: {
-             all: ['test/test_*.js']
-         },
+  // Project configuration.
+  grunt.initConfig({
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp']
+    },
+    nodeunit: {
+      all: ['test/test_*.js']
+    },
 
-         // Configuration to be run (and then tested).
-         sitespeedio: {
-             default_options: {
-                 options: {}
-             }
-         }
+    // Configuration to be run (and then tested).
+    sitespeedio: {
+      default_options: {
+        options: {
+        }
+      }
+    }
 
-     });
+  });
 
-     // Actually load this plugin's task(s).
-     grunt.loadTasks('tasks');
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
 
-     // These plugins provide necessary tasks.
-     grunt.loadNpmTasks('grunt-contrib-clean');
-     grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-     // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-     // plugin's task(s), then test the result.
-     grunt.registerTask('test', ['clean', 'sitespeedio']);
-     grunt.registerTask('test_acceptance', ['nodeunit:all']);
-     grunt.registerTask('default', 'test');
- };
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'sitespeedio']);
+  grunt.registerTask('test_acceptance', ['nodeunit:all']);
+  grunt.registerTask('default', 'test');
+};
