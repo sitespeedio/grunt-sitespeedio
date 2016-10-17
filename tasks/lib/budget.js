8593c60ac4724ccef607eac25744216ea0b4a5cc
soulgalore <peter@soulgalore.com>
2016-10-17 10:35:08 +0200

    use sitespeed.io 4.0 for the budget

--- tasks/lib/budget.js
+++ tasks/lib/budget.js
@@ -1,49 +1,35 @@
-/**
- * grunt-sitespeed.io (http://www.sitespeed.io)
- * Copyright (c) 2015, Peter Hedenskog, Tobias Lidskog
- * and other contributors
- * Released under the Apache 2.0 License
- */
 'use strict';
 
-exports.checkBudget = function(data, grunt) {
-	// lets get the budget!
-	grunt.log.ok('------------------------------------------------- Check budget');
-	var failing = false;
-	var showFailedOnly = grunt.config.get('showFailedOnly');
+exports.checkBudget = (result, grunt) => {
+  // lets get the budget!
+  grunt.log.ok('------------------------------------------------- Check budget');
+  const showFailedOnly = grunt.config.get('showFailedOnly');
 
-	var noPassedTests = 0;
-	var noFailingTests = 0;
-	var noSkippedTests = 0;
+  let noPassedTests = 0;
+  let noFailingTests = 0;
 
-	grunt.log.ok((showFailedOnly ? 'Will show only failing test.' : 'Show both failing and passing tests.') +
-		' Change this by set Grunt config showFailedOnly to true');
+  grunt.log.ok((showFailedOnly ? 'Will show only failing test.' : 'Show both failing and passing tests.') +
+    ' Change this by set Grunt config showFailedOnly to true');
 
-	grunt.log.ok('the showFailedOnly is:' + showFailedOnly);
+  if (!showFailedOnly) {
+    for (const url of Object.keys(result.working)) {
+      for (const budgetResult of result.working[url]) {
+        grunt.log.ok('The budget for ' + budgetResult.type + '.' + budgetResult.metric + ' ' + url + ' passed [' + budgetResult.value +
+          ']');
+        noPassedTests += 1;
+      }
+    }
+  }
+  for (const url of Object.keys(result.failing)) {
+    for (const budgetResult of result.failing[url]) {
+      grunt.log.error('[FAILED] The budget for ' + budgetResult.type + '.' + budgetResult.metric + ' ' + url + ' failed. [' + budgetResult.value + ' limit ' + budgetResult.limitType + ' ' + budgetResult.limit + ']');
+      noFailingTests += 1;
+    }
+  }
 
-	data.budget.forEach(function(result) {
-		if (result.skipped) {
-			noSkippedTests++;
-			if (!showFailedOnly)
-				grunt.log.ok('Skipping ' + result.title + ' ' + result.url + ' ' + ' value [' + result.value +
-					']');
-		} else if (result.isOk) {
-			noPassedTests++;
-			if (!showFailedOnly) {
-				grunt.log.ok('The budget for ' + result.title + ' ' + result.url + ' passed [' + result.value +
-					']');
-			}
-		} else {
-			noFailingTests++;
-			failing = true;
-			grunt.log.error('[FAILED] The budget for ' + result.title + ' ' + result.url + ' failed. ' + result.description);
-		}
-	});
+  grunt.log.ok('We got ' + noPassedTests + ' passing tests, ' + noFailingTests + ' failing');
 
-	grunt.log.ok('We got ' + noPassedTests + ' passing tests, ' + noFailingTests + ' failing' + ((
-		noSkippedTests > 0) ? ' ' + noSkippedTests + ' skipped tests' : '.'));
+  grunt.log.ok('------------------------------------------------- Finished checking budget');
 
-	grunt.log.ok('------------------------------------------------- Finished checking budget');
-
-	return failing;
-}
+  return noFailingTests > 0;
+};
--- tasks/sitespeedio.js
+++ tasks/sitespeedio.js
@@ -1,48 +1,25 @@
-/**
- * grunt-sitespeed.io (http://www.sitespeed.io)
- * Copyright (c) 2015, Peter Hedenskog, Tobias Lidskog
- * and other contributors
- * Released under the Apache 2.0 License
- */
-
 'use strict';
 
-var fs = require('fs'),
-  path = require('path'),
-  EOL = require('os').EOL,
-  budget = require('./lib/budget');
+const sitespeed = require('sitespeed.io/lib/sitespeed');
+const tmp = require('temporary');
+const budget = require('./lib/budget');
 
-module.exports = function(grunt) {
-
-  grunt.registerMultiTask('sitespeedio', 'Analyze your sites web performance', function() {
-
-    var tmp = require('temporary');
-    var dir = new tmp.Dir();
-
-    var options = this.options({
-      resultBaseDir: dir.path,
-      html: true
+module.exports = (grunt) => {
+  grunt.registerMultiTask('sitespeedio', 'Analyze your sites web performance', function analyze() {
+    const dir = new tmp.Dir();
+    const options = this.options({
+      outputFolder: dir.path
     });
 
-    // special handling for reading file with urls
-    // in sitespeed.io we read the file within the cli,
-    // so we need to do it from outside when we run without the
-    // cli
-    if (options.file) {
-      readFile(options);
-    }
+    const done = this.async();
 
-    var done = this.async();
-    var Sitespeed = require('sitespeed.io/lib/sitespeed');
-    var sitespeed = new Sitespeed();
-
-    sitespeed.run(options, function(err, data) {
-
-      if (err) {
+    sitespeed.run(options).then((result) => {
+      if (result.errors.length > 0) {
         done(false);
-      } else if (data && data.budget) {
-        var isFailing = budget.checkBudget(data, grunt);
+      } else if (result.budgetResult) {
+        const isFailing = budget.checkBudget(result.budgetResult, grunt);
         if (isFailing) {
+          // we got failing!!
           done(false);
         } else {
           done();
@@ -53,20 +30,3 @@ module.exports = function(grunt) {
     });
   });
 };
-
-function readFile(options) {
-
-  // absoulute or relative
-  var fullPathToFile = (options.file.charAt(0) === path.sep) ? options.file : path.join(process.cwd(),
-    path.sep, options.file);
-
-  var data = fs.readFileSync(fullPathToFile);
-  var urls = data.toString().split(EOL);
-  urls = urls.filter(function(l) {
-    return l.length > 0;
-  });
-  // we clean the file in the config to make
-  // it look that we are feeding with URL array
-  options.urls = urls;
-  options.file = undefined;
-}
